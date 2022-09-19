27aeb4347f8636171e13879800e65b3fd1c1cfc4
OCKing <51097023+KDW1@users.noreply.github.com>
2022-09-18 19:05:59 -0500

    Added JSON

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const { json } = require('express')
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 3000
 app.set('view engine', 'ejs')
 
+let jsonData = fs.writeFile("./roster.json", "utf8", (err, jsonString) => {
+    if(err) {
+        console.log("Error: " + err)
+    } else {
+        return jsonData
+    }
+})
+console.log(jsonData)
 app.get('/', (req, res) => {
     res.render('index')
 })
